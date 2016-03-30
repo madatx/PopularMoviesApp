fed033882d59abd58c1d941d2f43847adbbf1906
Anna <atx@MacBook-Pro-Anna.local>
2016-03-30 22:07:02 +0300

    added apikey getter

---
+++ app/src/main/java/com/example/atx/popularmoviesapp/utils/Util.java
@@ -0,0 +1,27 @@
+package com.example.atx.popularmoviesapp.utils;
+
+import android.content.Context;
+
+import com.example.atx.popularmoviesapp.R;
+
+public class Util {
+
+    private static String getApiKeyFromFile(Context context){
+        try {
+            String line = context.getString(R.string.apikey);
+            return line;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String apiKey = null;
+
+    public static String getApiKey(Context context){
+        if (apiKey == null){
+            apiKey = getApiKeyFromFile(context);
+        }
+        return apiKey;
+    }
+}
